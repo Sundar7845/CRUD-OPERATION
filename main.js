a50bf4867ed8ff2c2e5662e1d5a6e1b8f07e166f
sundar7854 <prathip221@gmail.com>
2026-02-17 10:28:06 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,32 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello CRUD" });
 });
 
+// CRUD functionality of movies
+
+// C - For Create a moive
+app.get("/movies", (req, res) => {
+
+
+});
+
+// U - For update a moive
+app.post("/movies", (req, res) => {
+
+
+});
+
+// D - For Create a moive
+app.put("/movies/:id", (req, res) => {
+
+
+});
+
+// C - For Create a moive
+app.delete("/movies/:id", (req, res) => {
+
+
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
